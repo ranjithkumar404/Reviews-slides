fceec9b14550c66fb8d8f6f92b653b3ec9edbd41
ranjithkumar404 <ranjithkumarshetty808@gmail.com>
2023-05-20 16:16:10 +0530

    new commit

--- src/People.jsx
+++ src/People.jsx
@@ -4,22 +4,34 @@ import { FaChevronLeft, FaChevronRight } from 'react-icons/fa'
 const People = () => {
     const [i, setI] = useState(0)
     const { id, name, image, about } = character[i]
-    const setIndex=(id)=>{
-       if(id<=5)
-           setI(id + 1)
-        else 
-           setI(0)
+    const setPrev = () => {
+
+        setI((i)=>{
+        if(i-1 <0)
+        return 4
+        else
+        return i-1
+        })
+
+    }
+    const setNext = () => {
+        setI((i) => {
+            if (i + 1 < character.length)
+                return i + 1
+            else
+                return 0
+        })
     }
 
     return (
-        <div className=' bg-slate-500/30 rounded-lg p-5 shadow-2xl  w-[500px] grid gap-3 place-items-center ' key={id}>
+        <div className=' bg-orange-500 rounded-lg p-5 shadow-xl  w-[500px] grid gap-3 text-white place-items-center ' key={id}>
             <img className='rounded-full' src={image} alt='' />
-            <p className='text-3xl font-bold text-black'>{name}</p>
+            <p className='text-3xl font-bold '>{name}</p>
             <p className='text-lg'>{about}</p>
-          <div className='flex'>
-                < FaChevronLeft  />
-                <FaChevronRight onClick={() => setIndex(id)} />
-          </div>
+            <div className='flex space-x-3 text-xl'>
+                < FaChevronLeft className='cursor-pointer' onClick={() => setPrev()} />
+                <FaChevronRight className='cursor-pointer' onClick={() => setNext()} />
+            </div>
         </div>
     )
 }
